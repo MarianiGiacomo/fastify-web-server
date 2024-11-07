586ac6dd744211bac6666b9eb325026035287cef
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-11-07 22:27:47 +0200

    serving static content

--- routes/hello/index.js
+++ routes/hello/index.js
@@ -1,20 +1,8 @@
 'use strict'
 
-const hello = `<html>
-  <head>
-    <style>
-      body { background: #333; margin: 1.25rem }
-      h1 { color: #EEE; font-family: sans-serif }
-    </style>
-  </head>
-  <body>
-    <h1>Hello World</h1>
-  </body>
-</html>`
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    reply.type('text/html')
-    return hello
+//So hello.html can be accessed both from /hello and /hello.html
+module.exports = async (fastify, opts) => {
+  fastify.get('/', async (request, reply) => {
+    return reply.sendFile('hello.html')
   })
 }
